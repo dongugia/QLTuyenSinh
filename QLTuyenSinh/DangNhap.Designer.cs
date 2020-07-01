8256905f1ed376d008028ea8dbb8afe6e26f2d78
dongugia <dongugia@gmail.com>
2020-07-01 17:40:02 +0900

    update

---
+++ QLTuyenSinh/DangNhap.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace QLTuyenSinh
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnDangNhap = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 25);
+            this.textBox1.MaxLength = 50;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 74);
+            this.textBox2.MaxLength = 50;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(173, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // BtnDangNhap
+            // 
+            this.BtnDangNhap.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.BtnDangNhap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnDangNhap.Font = new System.Drawing.Font("Arial Unicode MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.BtnDangNhap.ForeColor = System.Drawing.Color.DarkRed;
+            this.BtnDangNhap.Image = global::QLTuyenSinh.Properties.Resources.DkSiken_logo;
+            this.BtnDangNhap.Location = new System.Drawing.Point(198, 40);
+            this.BtnDangNhap.Name = "BtnDangNhap";
+            this.BtnDangNhap.Size = new System.Drawing.Size(153, 49);
+            this.BtnDangNhap.TabIndex = 2;
+            this.BtnDangNhap.Text = "Đăng Nhập";
+            this.BtnDangNhap.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnDangNhap.UseVisualStyleBackColor = true;
+            this.BtnDangNhap.Click += new System.EventHandler(this.BtnDangNhap_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.BtnDangNhap);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox1.Location = new System.Drawing.Point(78, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(369, 117);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(36, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CHƯƠNG TRÌNH QUẢN LÝ TUYỂN SINH ĐẠI HỌC- CAO ĐẲNG";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(200, 50);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.Visible = false;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 257);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "DangNhap";
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.DangNhap_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnDangNhap;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ QLTuyenSinh/DangNhap.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTuyenSinh
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int i = 10;
+            label1.Left += 1;
+            if (label1.Left > this.Width - label1.Width || label1.Width <= 0) i = -1;
+
+        }
+        private void TruyCap(string truyvan)
+        {
+            SqlConnection con = null;
+            SqlDataAdapter daTbl = null;
+            DataTable dtTbl = null;
+
+            string ketnoi = @"Data Source=DK0626-PC\SQLEXPRESS;Initial Catalog=QLTuyenSinh;Integrated Security=True";
+            try
+            {
+                con = new SqlConnection(ketnoi);
+                daTbl = new SqlDataAdapter(truyvan, con);
+                dtTbl = new DataTable();
+                dtTbl.Clear();
+                daTbl.Fill(dtTbl);
+
+                dataGridView1.DataSource = dtTbl;
+                daTbl.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        private void DangNhap_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+
+        private void BtnDangNhap_Click(object sender, EventArgs e)
+        {
+            TruyCap("select * from Users");
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                if (textBox1.Text == dataGridView1.Rows[i].Cells[1].Value.ToString() && dataGridView1.Rows[i].Cells[2].Value.ToString() == textBox2.Text
+                    && dataGridView1.Rows[i].Cells[3].Value.ToString() == "0")
+                {
+                    textBox1.Text = textBox2.Text = "";
+                    MessageBox.Show("Bạn đã đăng nhập thành công với tư cách là quản trị viên!");
+                }
+                else if (textBox1.Text == dataGridView1.Rows[i].Cells[1].Value.ToString() && dataGridView1.Rows[i].Cells[2].Value.ToString() == textBox2.Text
+                    && dataGridView1.Rows[i].Cells[3].Value.ToString() == "1")
+                {
+                    textBox1.Text = textBox2.Text = "";
+                    MessageBox.Show("Bạn đã đăng nhập thành công với tư cách là nhân viên!");
+                }
+                else if(textBox1.Text != dataGridView1.Rows[i].Cells[1].Value.ToString() ^ dataGridView1.Rows[i].Cells[2].Value.ToString() != textBox2.Text)
+                {
+                    textBox1.Focus();
+                    MessageBox.Show("Đăng nhập không thành công, \r\nXin kiểm tra lại!");
+                }
+            }
+        }
+    }
+}
